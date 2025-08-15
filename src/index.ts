082ae1345f538d693710e3e9282ec9e09e4499d0
Pranav K <pranavkcse@gmail.com>
2025-08-15 14:36:36 +0530

    feat: add authentication middleware and update scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import auth_routes from "@module/auth/route";
+import { authMiddleware } from "@middleware/auth";
 
 const app = new Hono();
 
@@ -12,5 +13,7 @@ app.get("/", (c) => {
 
 // Auth routes
 app.route("/api/", auth_routes);
+app.use(authMiddleware)
+
 
 export default app;
---
+++ src/middleware/auth.ts
@@ -0,0 +1,46 @@
+import { createMiddleware } from "hono/factory";
+import { auth, type AuthType } from "@shared/auth";
+import { HTTPException } from "hono/http-exception";
+
+// Extend the Hono context to include user and session
+export type Variables = {
+  user: AuthType["user"];
+  session: AuthType["session"];
+};
+
+
+export const authMiddleware = createMiddleware<{ Variables: Variables }>(
+  async (c, next) => {
+    const session = await auth.api.getSession({
+      headers: c.req.raw.headers,
+    });
+
+    if (!session) {
+      throw new HTTPException(401, { message: "Unauthorized" });
+    }
+
+    c.set("user", session.user);
+    c.set("session", session.session);
+
+    await next();
+  }
+);
+
+export const optionalAuthMiddleware = createMiddleware<{
+  Variables: Partial<Variables>;
+}>(async (c, next) => {
+  try {
+    const session = await auth.api.getSession({
+      headers: c.req.raw.headers,
+    });
+
+    if (session) {
+      c.set("user", session.user);
+      c.set("session", session.session);
+    }
+  } catch (error) {
+    // Continue without authentication
+  }
+
+  await next();
+});
